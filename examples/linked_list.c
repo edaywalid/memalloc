c16f20c327cdbb72900a7644fec6d2222783809e
edaywalid <becharwalid9@gmail.com>
2024-07-23 15:37:57 +0100

    linked_list implementation using my custom memallocator

---
+++ examples/linked_list.c
@@ -0,0 +1,44 @@
+#include "../memalloc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node *list;
+typedef struct Node {
+  int val;
+  list next;
+} Node;
+
+list push(list *root, int val) {
+  list new_node = (list)malloc(sizeof(Node));
+  new_node->val = val;
+  new_node->next = *root;
+  *root = new_node;
+  return new_node;
+}
+
+int pop(list *root) {
+  if (root == NULL) {
+    exit(1);
+  }
+  int val = (*root)->val;
+  list tmp = *root;
+  *root = (*root)->next;
+  free(tmp);
+  return val;
+}
+void print_list(list root) {
+  while (root) {
+    printf("%d ", root->val);
+    root = root->next;
+  }
+  printf("\n");
+}
+
+list init() { return NULL; }
+int main() {
+  list head = init();
+  head = push(&head, 5);
+  head = push(&head, 3);
+
+  print_list(head);
+}
