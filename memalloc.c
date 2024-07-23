829821e2e127c4391032583e07f34c03d7cbca19
edaywalid <becharwalid9@gmail.com>
2024-07-23 15:36:05 +0100

    defined find_free_block function

--- memalloc.c
+++ memalloc.c
@@ -1,1 +1,17 @@
 #include "./memalloc.h"
+#include "stdlib.h"
+#include <unistd.h>
+
+union Header *head = NULL, *tail = NULL;
+
+union Header *find_free_block(size_t bsize) {
+  union Header *current = head;
+  while (current) {
+    if (current->s.free && current->s.size == bsize) {
+      return current;
+    }
+    current = current->s.next;
+  }
+  return NULL;
+}
+
