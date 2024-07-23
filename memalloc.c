95cec253ae8d58ac3e8779d5d0a293110a839b59
edaywalid <becharwalid9@gmail.com>
2024-07-23 15:36:22 +0100

    defined malloc function using sbrk() syscall

--- memalloc.c
+++ memalloc.c
@@ -15,3 +15,31 @@ union Header *find_free_block(size_t bsize) {
   return NULL;
 }
 
+void *malloc(size_t size) {
+  if (!size) {
+    return NULL;
+  }
+  size_t full_size = size + sizeof(union Header);
+
+  union Header *header = find_free_block(size);
+  if (header) {
+    header->s.free = 0;
+    return (void *)(header + 1);
+  }
+
+  union Header *block = sbrk(full_size);
+  block->s.size = full_size;
+  block->s.free = 0;
+  block->s.next = NULL;
+
+  header = block;
+  if (!head) {
+    head = header;
+  }
+  if (tail) {
+    tail->s.next = header;
+  }
+  tail = header;
+
+  return (void *)(header + 1);
+}
